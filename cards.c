4af628bc497ce0a9cc0c1b3d7d999cfd16e03ce5
Zachary Anglin <zachary@anglin.com>
2020-07-29 09:07:16 -0400

    Finished chapter one

--- cards.c
+++ cards.c
@@ -8,21 +8,39 @@
 
 int main()
 {
-	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
-	int val = 0;
-	if (card_name[0] == 'K') {
-		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
-		val = 11;
-	} else {
-		val = atoi(card_name);
+    char card_name[3];
+    int count = 0;
+    do {
+    	puts("Enter the card_name: ");
+    	scanf("%2s", card_name);
+    	int val = 0;
+    	switch(card_name[0]) {
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		case 'X':
+			continue;
+		default:
+			val = atoi(card_name);
+	
+		if (val < 1 || val > 10) {
+			puts("Error! val is out of range!");
+			continue;
+		}
 	}
-	printf("The card value is: %i\n", val);
-	return 0;
+    	
+	/* Check if the value is 3 to 6 */
+    	if ((val > 2) && (val < 7))
+    		count++;
+	else if (val == 10) {
+		count--;
+	}
+	printf("Current count: %i\n", count);
+    } while (card_name[0] != 'X');
+    return 0;
 }
